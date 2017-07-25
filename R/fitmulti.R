6f48d7ab7424e068cd4be1d7236893ba6c266d75
Maryclare Griffin <maryclaregriffin@gmail.com>
2017-07-25 11:15:13 -0400

    Updated to allow for resampling of noise variance and variance-covariance matrix of coefficients (three structures)

--- R/fitmulti.R
+++ R/fitmulti.R
@@ -1,3 +1,33 @@
+sample.sigma.z.inv <- function(y, X, old.u, old.v, S.u.i, S.v.i, pr.a = 1/2, pr.b = 1/2) {
+  p <- nrow(old.u)
+  n <- length(y)
+  beta <- old.u*old.v
+  ssr <- crossprod(y - crossprod(t(X), beta))
+  r.u <- crossprod(old.u, crossprod(S.u.i, old.u))
+  r.v <- crossprod(old.v, crossprod(S.v.i, old.v))
+  b <- as.numeric(ssr + r.u + r.v)/2 + pr.b
+  a <- n/2 + p/2 + p/2 + pr.a
+  return(rgamma(1, shape = a, rate = b))
+}
+
+sample.Sigma.inv <- function(old, sigma.sq.z, pr.V.inv = diag(nrow(old)),
+                             pr.df = nrow(old) + 2, str) {
+  p <- nrow(old)
+  if (str == "uns") {
+    V.inv <- tcrossprod(old)/sigma.sq.z + pr.V.inv
+    df <- ncol(old) + p + 2
+    return(rWishart(1, df, solve(V.inv))[, , 1])
+  } else if (str == "het") {
+    b <- apply(old, 1, function(x) {sum(x^2)})/(2*sigma.sq.z) + diag(pr.V.inv)/2
+    a <- rep(ncol(old), nrow(old))/2 + pr.df/2
+    return(diag(rgamma(p, shape = a, rate = b)))
+  } else if (str == "con") {
+    b <- sum(apply(old, 1, function(x) {sum(x^2)}))/(2*sigma.sq.z) + sum(diag(pr.V.inv))/2
+    a <- sum(rep(ncol(old), nrow(old)))/2 + p*pr.df/2
+    return(rgamma(1, shape = a, rate = b)*diag(p))
+  }
+}
+
 sample.uv <- function(old.v, sigma.sq.z,
                       Sigma.u.inv, Sigma.v.inv, XtX, Xty) {
   p <- length(old.v)
@@ -33,88 +63,58 @@ sample.uv <- function(old.v, sigma.sq.z,
 }
 #' @export
 mp.mcmc <- function(X, y, sigma.sq.z,
-                      Sigma.u.inv = diag(ncol(X)), Sigma.v.inv = diag(ncol(X)), num.samp = 10000) {
+                      Sigma.u.inv = NULL, Sigma.v.inv = NULL, num.samp = 10000,
+                    str = "uns") {
 
   p <- ncol(X)
-  eps <- 10^(-3)
 
   XtX <- crossprod(X)
   Xty <- crossprod(X, y)
 
-  old.v <- crossprod(solve(XtX + eps*diag(p)), Xty)
+  eps <- -min(eigen(XtX)$values) + 1
+  ridge.est <- crossprod(solve(XtX + eps*diag(p)), Xty)
+  old.v <- sqrt(abs(ridge.est))
+  old.u <- sign(ridge.est)*sqrt(abs(ridge.est))
 
-  samples <- array(dim = c(num.samp, p))
+  samples.beta <- array(dim = c(num.samp + 1, p))
+  samples.Sigma <- array(dim = c(num.samp + 1, p^2))
+  samples.sigma.sq.z <- array(dim = c(num.samp + 1, 1))
 
-  for (i in 1:num.samp) {
-    s <- sample.uv(old.v, sigma.sq.z,
-                   Sigma.u.inv, Sigma.v.inv, XtX, Xty)
-    samples[i, ] <- s[, 1]*s[, 2]
-    old.v <- s[, 2]
+  if (!is.null(Sigma.u.inv)) {
+    S.u.i <- Sigma.u.inv
   }
-
-  return(samples)
-
-}
-
-pv.bxy <- function(v, beta, sigma.sq.z = 1, Sigma.u = diag(length(beta)),
-                   Sigma.v.inv = diag(length(beta)), log = TRUE) {
-  log.lik <- -log(det(Sigma.u*(v%*%t(v))))/2 -
-    sum(diag(t(v)%*%Sigma.v.inv%*%v))/(2*sigma.sq.z) -
-    sum(diag(t(beta)%*%solve(Sigma.u*(v%*%t(v)))%*%beta))/(2*sigma.sq.z)
-  if (log) {
-    return(log.lik)
+  if (!is.null(Sigma.v.inv)) {
+    S.v.i <- Sigma.v.inv
+  }
+  if (!is.null(sigma.sq.z)) {
+    s.s.z <- sigma.sq.z
   } else {
-    return(exp(log.lik))
+    s.s.z <- 1
   }
-}
-#' @export
-mp.em <- function(Q, l, sigma.sq.z = 1,
-                    Sigma.u.inv = diag(ncol(Q)), Sigma.v.inv = diag(ncol(Q)),
-                    tol = 10^(-7),
-                    mh = list("samps" = 10000, "burn" = 1000),
-                    print.iter = FALSE, tune = 0.5, max.iter = 1000) {
 
-  p <- length(l)
-  beta.old <- rep(Inf, p)
-  beta.new <- crossprod(solve(Q + 0.00001*diag(p)), l)
-  j <- 1
-  while (j <= max.iter & sum((beta.old - beta.new)^2) > tol) {
-    v.old <- rep(1, p)
-    samps <- mh[["samps"]]
-    burn <- mh[["burn"]]
-    vs <- matrix(nrow = samps, ncol = p)
-    acc <- matrix(nrow = samps, ncol = p)
-    for (i in 1:(samps + burn)) {
-      accep <- rep(0, p)
-      for (k in 1:p) {
-        v.new <- v.old
-        v.new[k] <- v.old[k] + tune*rnorm(1)
-        pr.new <- pv.bxy(v.new, beta = beta, sigma.sq.z = 1, Sigma.u = solve(Sigma.u.inv),
-                         Sigma.v.inv = solve(Sigma.v))
-        pr.old <- pv.bxy(v.old, beta = beta, sigma.sq.z = 1, Sigma.u = solve(Sigma.u.inv),
-                         Sigma.v.inv = solve(Sigma.v))
-        if (pr.new > pr.old | runif(1) <= exp(pr.new - pr.old)) {
-          accep[k] <- 1
-          v.old <- v.new
-        }
-      }
-      if (i > burn) {
-        vs[i - burn, ] <- v.old
-        acc[i - burn, ] <- accep
-      }
+  for (i in 1:(num.samp + 1)) {
+    if (is.null(Sigma.u.inv)) {
+      S.u.i <- sample.Sigma.inv(old = old.u, sigma.sq.z = s.s.z, str = str)
     }
-    if(print.iter) {
-      cat("j = ", j, "\n")
-      cat("Diff=", sum((beta.old - beta.new)^2), "\n")
-      cat("Acc=", min(colMeans(acc)), "\n")
+    if (is.null(Sigma.v.inv)) {
+      S.v.i <- sample.Sigma.inv(old = old.v, sigma.sq.z = s.s.z, str = str)
     }
+    s <- sample.uv(old.v, s.s.z,
+                   S.u.i, S.v.i, XtX, Xty)
+    samples.beta[i, ] <- s[, 1]*s[, 2]
+    samples.Sigma[i, ] <- as.vector(solve(S.u.i)*solve(S.v.i))
+    old.u <- s[, 1, drop = FALSE]
+    old.v <- s[, 2, drop = FALSE]
+    if (is.null(sigma.sq.z)) {
+      s.s.z <- 1/sample.sigma.z.inv(y = y, X = X, old.u = old.u, old.v = old.v,
+                                    S.u.i = S.u.i, S.v.i = S.v.i)
+    }
+    samples.sigma.sq.z[i, ] <- s.s.z
 
-    e.Sigma.u.vvt.inv <- matrix(rowMeans(apply(vs, 1, function(x) {as.vector(solve(Sigma.u*(x%*%t(x))))})), nrow = p, ncol = p)
-
-    beta.old <- beta.new
-    beta.new <- crossprod(solve(Q + e.Sigma.u.vvt.inv),l)
-    j <- j + 1
   }
-  return(list("beta" = beta.new, "conv" = sum((beta.old - beta.new)^2) <= tol,
-              "vs" = vs))
+
+  return(list("beta" = samples.beta[-1, ],
+              "Sigma" = samples.Sigma[-1, ],
+              "sigma.sq.z" = samples.sigma.sq.z[-1]))
+
 }
